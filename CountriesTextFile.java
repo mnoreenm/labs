13c67edcefbec299fdbcdbb92dfc3422a40dfcfa
Sadia Noreen <mnoreenm@yahoo.com>
2016-09-15 13:43:32 -0400

    updated Lab 15

--- CountriesTextFile.java
+++ CountriesTextFile.java
@@ -1,7 +1,9 @@
     import java.io.BufferedReader;
-	import java.io.File;
+import java.io.BufferedWriter;
+import java.io.File;
 	import java.io.FileReader;
-	import java.io.IOException;
+import java.io.FileWriter;
+import java.io.IOException;
 	import java.io.PrintWriter;
 	import java.nio.file.Files;
 	import java.nio.file.Path;
@@ -78,7 +80,10 @@
 				}
 				
 				File file = filePath.toFile();
-				PrintWriter writer = new PrintWriter(file);
+				PrintWriter writer = new PrintWriter(
+				                     new BufferedWriter(
+				                     new FileWriter(file,true)));
+				 
 				
 				for (String country : countries) {
 					writer.println(country);
